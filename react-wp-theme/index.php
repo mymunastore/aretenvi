fe5ae485fd2c71a96a27c0d506cdcfba894f29b9
Kingsley Munachi <info@mymuna.store>
2025-10-01 13:51:59 -0400

    Added index.php

---
+++ react-wp-theme/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Main Template File
+ *
+ * This is the main template that loads the React application
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+get_header();
+?>
+
+<div id="root"></div>
+
+<?php
+get_footer();
